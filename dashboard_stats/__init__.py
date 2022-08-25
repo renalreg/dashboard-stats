7d1e40eeedd3a2882721d722205c4a009ba2e198
phil <Philip.Main@renalregistry.nhs.uk>
2022-08-25 13:44:05 +0100

    seperated out pydantic classes

--- dashboard_stats/__init__.py
+++ dashboard_stats/__init__.py
@@ -3,3 +3,6 @@ from dashboard_stats import demographics
 
 # Utility functions to handle bitty things
 from dashboard_stats import utils
+
+# pydantic classes to return nice plotly friendly output
+from dashboard_stats import output_classes
