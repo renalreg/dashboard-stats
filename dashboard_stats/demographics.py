2c5baed34aba0fe114d4bd01c3e993f734db164f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-08-31 11:21:30 +0100

    Pylint fixes

--- dashboard_stats/demographics.py
+++ dashboard_stats/demographics.py
@@ -1,16 +1,22 @@
-from ukrdc_sqla.ukrdc import Patient, Treatment
-from sqlalchemy import select
-import dashboard_stats.models as oc
-
-import pandas as pd
 import datetime as dt
 
+import pandas as pd
+from sqlalchemy.orm import Session
+from ukrdc_sqla.ukrdc import Patient, Treatment
 
-class Demog:
+from dashboard_stats.models import Bar, BarList, Data, Layout
+
+
+class PatientDemographicStats:
     """Calculates the demographics information based on the personal infomation listed in the patient table"""
 
-    def __init__(self, session, facility, date=dt.datetime.today()):
-        """Initialises the Demog class
+    def __init__(
+        self,
+        session: Session,
+        facility: str,
+        date: dt.datetime = dt.datetime.today(),
+    ):
+        """Initialises the PatientDemographicStats class and immediately runs the relevant query
 
         Args:
             session (SQLAlchemy session): Connection to database to calculate statistic from.
@@ -18,13 +24,42 @@ class Demog:
             date (datetime, optional): Date to calculate at. Defaults to today.
         """
 
-        self.session = session
-        self.facility = facility
-        self.date = date
+        self.session: Session = session
+        self.facility: str = facility
+        self.date: dt.datetime = date
+
+        # Immediately run the patient cohort query, so it's ready for re-use
+        self.patient_cohort: pd.DataFrame = self._extract_patient_cohort()
+
+    def _extract_patient_cohort(self) -> pd.DataFrame:
+        """
+        Extracts the patient cohort from the database into a pandas dataframe
+        """
+        # select all patients with modalities that haven't finished
+        patient_query = (
+            self.session.query(Patient)
+            .join(Treatment, Treatment.pid == Patient.pid)
+            .filter(
+                Treatment.health_care_facility_code == self.facility,
+                (Treatment.from_time < self.date)
+                & ((Treatment.to_time is None) | (Treatment.to_time >= self.date)),
+                (Patient.death_time >= self.date) | (Patient.death_time is None),
+            )
+        )
+
+        return pd.read_sql(patient_query, self.session.bind)
+
+    def _make_patient_histogram(self, group: str) -> pd.DataFrame:
+        return (
+            self.patient_cohort[["pid", group]]
+            .drop_duplicates()
+            .groupby([group])
+            .count()
+        )
 
     def patient_info(self):
         """
-        Grab info on current patients by running select on Patient table for patient
+        Extract demographic statistics from the patient cohort dataframe
         TODO:
             - Test if using pandas read_sql is slowing the code down
             - Security implications. Should patient cohort be a private variable?
@@ -34,77 +69,54 @@ class Demog:
             - validation method to pydantic class...for example that the sum of numbers should equal the total population
         """
 
-        # select all patients with modalities that haven't finished
-        patient_query = (
-            select(Patient)
-            .join(Treatment, Treatment.pid == Patient.pid)
-            .filter(
-                Treatment.health_care_facility_code == self.facility,
-                (Treatment.from_time < self.date)
-                & ((Treatment.to_time == None) | (Treatment.to_time >= self.date)),
-                (Patient.death_time >= self.date) | (Patient.death_time == None),
-            )
-        )
-
-        self.patient_cohort = pd.read_sql(patient_query, self.session.bind)
-
         # Crunch the numbers and make dataframes to produce "histograms" to display idividual bits of data
         pop_size = len(self.patient_cohort[["pid"]].drop_duplicates())
 
-        make_hist = (
-            lambda group: self.patient_cohort[["pid", group]]
-            .drop_duplicates()
-            .groupby([group])
-            .count()
-        )
-
-        gender = make_hist("gender")
-        birth_country = make_hist("countryofbirth")
-        primary_language = make_hist("primarylanguagecode")
-        ethnic_group_code = make_hist("ethnicgroupcode")
-        occupation = make_hist("occupationcode")
+        gender = self._make_patient_histogram("gender")
+        birth_country = self._make_patient_histogram("countryofbirth")
+        primary_language = self._make_patient_histogram("primarylanguagecode")
+        ethnic_group_code = self._make_patient_histogram("ethnicgroupcode")
+        occupation = self._make_patient_histogram("occupationcode")
 
         # Build output object
 
-        self.patient_demog = oc.BarList(
+        return BarList(
             pop=pop_size,
             bars=[
-                oc.Bar(
-                    data=oc.Data(
-                        labels=[item for item in gender.index],
+                Bar(
+                    data=Data(
+                        labels=list(gender.index),
                         values=[item[0] for item in gender.values],
                     ),
-                    layout=oc.Layout(title="Gender"),
+                    layout=Layout(title="Gender"),
                 ),
-                oc.Bar(
-                    data=oc.Data(
-                        labels=[item for item in birth_country.index],
+                Bar(
+                    data=Data(
+                        labels=list(birth_country.index),
                         values=[item[0] for item in birth_country.values],
                     ),
-                    layout=oc.Layout(title="Birth Country"),
+                    layout=Layout(title="Birth Country"),
                 ),
-                oc.Bar(
-                    data=oc.Data(
-                        labels=[item for item in primary_language.index],
+                Bar(
+                    data=Data(
+                        labels=list(primary_language.index),
                         values=[item[0] for item in primary_language.values],
                     ),
-                    layout=oc.Layout(title="Primary Language"),
+                    layout=Layout(title="Primary Language"),
                 ),
-                oc.Bar(
-                    data=oc.Data(
-                        labels=[item for item in ethnic_group_code.index],
+                Bar(
+                    data=Data(
+                        labels=list(ethnic_group_code.index),
                         values=[item[0] for item in ethnic_group_code.values],
                     ),
-                    layout=oc.Layout(title="Ethnic Group"),
+                    layout=Layout(title="Ethnic Group"),
                 ),
-                oc.Bar(
-                    data=oc.Data(
-                        labels=[item for item in occupation.index],
+                Bar(
+                    data=Data(
+                        labels=list(occupation.index),
                         values=[item[0] for item in occupation.values],
                     ),
-                    layout=oc.Layout(title="Occupation"),
+                    layout=Layout(title="Occupation"),
                 ),
             ],
         )
-
-        return self.patient_demog
