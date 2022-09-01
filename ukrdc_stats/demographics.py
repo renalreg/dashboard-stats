6b97e667099e83523a524ca0003a94d9e3b56902
phil <Philip.Main@renalregistry.nhs.uk>
2022-09-01 12:25:32 +0100

    working demographic stats

--- ukrdc_stats/demographics.py
+++ ukrdc_stats/demographics.py
@@ -5,8 +5,14 @@ import pandas as pd
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from ukrdc_sqla.ukrdc import Patient, Treatment
+from sqlalchemy import select, and_
 
-from .models.generic_2d import Labelled2d, Labelled2dData, Labelled2dMetadata
+from .models.generic_2d import (
+    Labelled2d,
+    Labelled2dData,
+    Labelled2dMetadata,
+    AxisLabels2d,
+)
 
 
 class DemographicsMetadata(BaseModel):
@@ -47,21 +53,25 @@ class DemographicsCalculator:
         """
         Extracts the patient cohort from the database into a pandas dataframe
         """
+
         # select all patients with modalities that haven't finished
         patient_query = (
-            self.session.query(Patient)
+            select(Patient)
             .join(Treatment, Treatment.pid == Patient.pid)
-            .filter(
-                Treatment.health_care_facility_code == self.facility,
-                (Treatment.from_time < self.date)
-                & ((Treatment.to_time is None) | (Treatment.to_time >= self.date)),
-                (Patient.death_time >= self.date) | (Patient.death_time is None),
+            .where(
+                and_(
+                    Treatment.health_care_facility_code == self.facility,
+                    (Treatment.from_time < self.date),
+                    (Treatment.to_time == None) | (Treatment.to_time >= self.date),
+                    (Patient.death_time >= self.date) | (Patient.death_time == None),
+                )
             )
         )
 
         return pd.read_sql(patient_query, self.session.bind)
 
     def _make_patient_histogram(self, group: str) -> pd.DataFrame:
+
         return (
             self.patient_cohort[["pid", group]]
             .drop_duplicates()
@@ -69,16 +79,12 @@ class DemographicsCalculator:
             .count()
         )
 
+    def hello(self):
+        print | ("helloworld")
+
     def extract_stats(self):
         """
         Extract demographic statistics from the patient cohort dataframe
-        TODO:
-            - Test if using pandas read_sql is slowing the code down
-            - Security implications. Should patient cohort be a private variable?
-            - Introduce pydantic to the fray
-            - map to more meaningful labels
-            - option of passing some sort of config which contains a list charts and types of chart to calculate?
-            - validation method to pydantic class...for example that the sum of numbers should equal the total population
         """
 
         # Crunch the numbers and make dataframes to produce "histograms" to display idividual bits of data
@@ -94,27 +100,43 @@ class DemographicsCalculator:
         return DemographicsStats(
             metadata=DemographicsMetadata(population=pop_size),
             gender=Labelled2d(
-                metadata=Labelled2dMetadata(title="Gender"),
+                metadata=Labelled2dMetadata(
+                    title="Gender Distribution",
+                    coding_standard_x="NHS_DATA_DICTIONARY",
+                    axis_titles=AxisLabels2d(x="Gender", y="No. of Patients"),
+                ),
                 data=Labelled2dData(
                     x=list(gender.index), y=[item[0] for item in gender.values]
                 ),
             ),
             birth_country=Labelled2d(
-                metadata=Labelled2dMetadata(title="Country of birth"),
+                metadata=Labelled2dMetadata(
+                    title="Country of birth",
+                    coding_standard_x="NHS_DATA_DICTIONARY",
+                    axis_titles=AxisLabels2d(x="Country", y="No. of Patients"),
+                ),
                 data=Labelled2dData(
                     x=list(birth_country.index),
                     y=[item[0] for item in birth_country.values],
                 ),
             ),
             primary_language=Labelled2d(
-                metadata=Labelled2dMetadata(title="Primary language"),
+                metadata=Labelled2dMetadata(
+                    title="Primary language",
+                    coding_standard_x="NHS_DATA_DICTIONARY",
+                    axis_titles=AxisLabels2d(x="Language", y="No. of Patients"),
+                ),
                 data=Labelled2dData(
                     x=list(primary_language.index),
                     y=[item[0] for item in primary_language.values],
                 ),
             ),
             ethnic_group_code=Labelled2d(
-                metadata=Labelled2dMetadata(title="Ethnic Group"),
+                metadata=Labelled2dMetadata(
+                    title="Ethnic Group",
+                    coding_standard_x="NHS_DATA_DICTIONARY",
+                    axis_titles=AxisLabels2d(x="Ethnicity", y="No. of Patients"),
+                ),
                 data=Labelled2dData(
                     x=list(ethnic_group_code.index),
                     y=[item[0] for item in ethnic_group_code.values],
