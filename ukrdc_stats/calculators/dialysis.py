f9504d16fec7563884f13c30b547e89d5ed8134e
phil <Philip.Main@renalregistry.nhs.uk>
2022-11-29 09:27:26 +0000

    added an example of how the description might work to the all home therapies statistics

--- ukrdc_stats/calculators/dialysis.py
+++ ukrdc_stats/calculators/dialysis.py
@@ -45,6 +45,74 @@ class DialysisStats(BaseModel):
     metadata: DialysisMetadata
 
 
+# placed long descriptions here as globals since they look kind of ugly inline
+ALL_PATIENTS_HOME_THERAPIES_DESC = """
+All Patients Home Therapies 
+---------------------------
+For a specified unit and time window contains:  
+    - Number of patients on Haemodialysis 
+      Defined as the number of patients registered for haemodialysis, haemofiltration, haemodiafiltration or ultrafiltration treatments.
+    - Number of patients on Peritoneal Dialysis
+      Defined as the number of patients registered for CAPD or APD treatments.
+    - Number of patients on home therapies 
+      Total number of patients on Peritoneal Dialysis as defined above combined with the number of patients on heamodialysis on home therapies.
+    - Number of patients on In centre therapies
+      Total number of patients registered for Haemodialysis in-centre
+
+These numbers are calculated from the Patient and Treatment records in the UKRDC. The patients are selected using the admission reason and the unit. The cohort is created from all the patients which are admitted for treatments: haemodialysis, haemofiltration, haemodiafiltration, ultrafiltration at the specified unit.
+
+For this patient cohort, the numbers of patients on Haemodialysis and Peritoneal dialysis is calculated. The patients are also split into home and incentre therapy groups with the intrinsic assumption that all patients on PD have home therapies.
+"""
+
+INCIDENT_HOME_THERAPIES_DESC = """
+Incident Patients Home Therapies 
+--------------------------------
+For a specified unit and time window contains:  
+    - Number of incident patients on Haemodialysis. 
+      Defined as the number of patients registered for haemodialysis, haemofiltration, haemodiafiltration or ultrafiltration treatments.
+    - Number of incident patients on Peritoneal Dialysis.
+      Defined as the number of patients registered for CAPD or APD treatments.
+    - Number of incident patients on home therapies. 
+      Total number of patients on Peritoneal Dialysis as defined above combined with the number of patients on heamodialysis on home therapies.
+    - Number of incident patients on In centre therapies.
+      Total number of patients registered for Haemodialysis in-centre
+
+Incidence in this instance is defined as the patients who begin treatment within the time window and have no other registered dialysis or transplant modalities. 
+"""
+
+PREVELENT_HOME_THERAPIES_DESC = """
+Prevalent Patients Home Therapies 
+--------------------------------
+For a specified unit and time window contains:  
+    - Number of prevalent patients on Haemodialysis. 
+      Defined as the number of patients registered for haemodialysis, haemofiltration, haemodiafiltration or ultrafiltration treatments.
+    - Number of prevalent patients on Peritoneal Dialysis.
+      Defined as the number of patients registered for CAPD or APD treatments.
+    - Number of prevalent patients on home therapies. 
+      Total number of patients on Peritoneal Dialysis as defined above combined with the number of patients on heamodialysis on home therapies.
+    - Number of prevalent patients on In centre therapies.
+      Total number of patients registered for Haemodialysis in-centre
+
+Incidence in this instance is defined as the patients who begin treatment within the time window and have no other registered dialysis or transplant modalities. 
+"""
+
+INCENTRE_DIALYSIS_FREQ_DESC = """
+
+
+
+"""
+
+INCIDENT_INITIAL_ACCESS_DESC = """
+Patients registered as having 
+"""
+
+DISCLAIMER = """
+DISCLAIMER
+The statistics presented here are are automatically calculated from data contained within the UKRDC and are presented for the convenience of data coordinators rather than an accurate audit of performance.   
+"""
+
+
+
 def _calculate_frequency(
     from_time: dt.datetime, to_time: dt.datetime, no_of_events: int
 ):
@@ -379,8 +447,8 @@ class DialysisStatsCalculator(AbstractFacilityStatsCalculator):
         return LabelledNetwork(
             metadata=NetworkMetaData(
                 title="Proportion of all Dialysis Patients on Home Therapies",
-                summary="",
-                description="",
+                summary="This shows the breakdown of all patients on dialysis by PD and HD, and by home thereapies and in-centre therapies.",
+                description=ALL_PATIENTS_HOME_THERAPIES_DESC + DISCLAIMER,
             ),
             node=all_patients_nodes,
             link=all_patients_vertices,
