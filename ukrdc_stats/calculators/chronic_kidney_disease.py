8d985a1dcee796ea85d824e043fd4588d6d1e319
phil <philip.main@renalregistry.nhs.uk>
2023-07-17 16:12:11 +0100

    initial commit

---
+++ ukrdc_stats/calculators/chronic_kidney_disease.py
@@ -0,0 +1,6 @@
+"""
+This module contains the calculators associated with chronic kidney disease. 
+That is patients which are identified as having kidney disease but are not in recipt KRT.
+"""
+
+
