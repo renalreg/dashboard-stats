d8c513b6703bcb63febbc2b0496814b76d910395
phil <philip.main@renalregistry.nhs.uk>
2025-04-16 08:42:33 +0100

    added modified script for batch extracting

--- scripts/extract_ckd.py
+++ scripts/extract_ckd.py
@@ -3,10 +3,10 @@ This example script shows how to extract a CKD cohort using the dashboard
 stats calculator to a csv file. 
 """
 
-
-
 import datetime as dt
+from pathlib import Path
 from rr_connection_manager import PostgresConnection
+from ukrdc_sqla.ukrdc import Facility
 from ukrdc_stats.calculators.ckd import PrevalentCKDCalculator
 
 # connection to the database
@@ -15,37 +15,42 @@ ukrdc_sessionmaker = ukrdc_conn.session_maker()
 
 
 # set parameters
-facility = "RCSLB"
-#facility = "RAQ01" 
-prevalence_point = dt.datetime(2023, 12, 31,0,0,0)
-report_file_path = f"Q:/UKRDC/Assessments/Phil_extracts/ckd_report_{facility}.csv"
+facilities = ["RHW01", "RAQ01"]
+prevalence_point = dt.datetime(2025, 3, 31,0,0,0)
+#prevalence_point = dt.datetime(2023, 12, 31,0,0,0)
+#report_file_path = Path("Q:/UKRDC/Assessments/Phil_extracts/")
+report_file_path = Path(".do_not_commit")
 
 with ukrdc_sessionmaker() as ukrdc_session:
-    # initialise the required calculator and extract the cohort from the
-    # database
-    calculator = PrevalentCKDCalculator(
-        session=ukrdc_session, 
-        facility=facility, 
-        prevalence_point=prevalence_point,
-    )
-    cohort = calculator.extract_patient_cohort()
-    
-    # generate a report from the extracted data
-    population, report = calculator.produce_report(
-        output_columns=[
-                "sendingfacility","birthtime", "deathtime", "fromtime",
-                "totime", "sex", "postcode", "ethnicgroupcode", 
-                "ethnicgroupdesc", "ukkaethnicity", "admitreasoncode",
-                "admitreasoncodestd", "admitreasondesc", "assessmentstart",
-                "assessmentend", "assessmenttypecode", "assessmenttypecodestd",
-                "assessmenttypecodedesc", "assessmentoutcomecode", 
-                "assessmentoutcomecodestd", "assessmentoutcomecodedesc", 
-                "serviceidcode_creat", "resultvalue_creat", 
-                "resultvalueunits_creat", "observationtime_creat", 
-                "serviceidcode_labegfr", "resultvalue_labegfr", 
-                "resultvalueunits_labegfr", "observationtime_labegfr",
-                "calculated_egfr", "externalid", "organization"
-            ]
+    for facility in facilities:
+        # initialise the required calculator and extract the cohort from the
+        # database
+        calculator = PrevalentCKDCalculator(
+            session=ukrdc_session, 
+            facility=facility, 
+            prevalence_point=prevalence_point,
         )
-    report.to_csv(report_file_path)
-    print(f"{population} CKD patients extracted to file {report_file_path}")
+        cohort = calculator.extract_patient_cohort()
+        
+        # generate a report from the extracted data
+        population, report = calculator.produce_report(
+            output_columns=[
+                    "sendingfacility","birthtime", "deathtime", "fromtime",
+                    "totime", "sex", "postcode", "ethnicgroupcode", 
+                    "ethnicgroupdesc", "ukkaethnicity", "admitreasoncode",
+                    "admitreasoncodestd", "admitreasondesc", "assessmentstart",
+                    "assessmentend", "assessmenttypecode", "assessmenttypecodestd",
+                    "assessmenttypecodedesc", "assessmentoutcomecode", 
+                    "assessmentoutcomecodestd", "assessmentoutcomecodedesc", 
+                    "serviceidcode_creat", "resultvalue_creat", 
+                    "resultvalueunits_creat", "observationtime_creat", 
+                    "serviceidcode_labegfr", "resultvalue_labegfr", 
+                    "resultvalueunits_labegfr", "observationtime_labegfr",
+                    "calculated_egfr", "externalid", "organization"
+                ]
+            )
+
+        # output results
+        output_path = report_file_path / Path(f"ckd_report_{facility}")
+        report.to_csv(output_path)
+        print(f"{population} CKD patients extracted to file {output_path}")
